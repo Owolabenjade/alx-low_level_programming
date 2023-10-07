1ff706aa761f041df63186fed184a1a9bd41f73e
Benjamin <owolabenjade@example.com>
2023-10-07 07:59:09 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function of the program.
+ * It uses the `printf` function to print the message
+ * "with proper grammar, but the outcome is a piece of art,"
+ * followed by a newline character.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
