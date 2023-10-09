8994eef7cad0172f07fd31c76c767aa3771626b2
Benjamin <owolabenjade@example.com>
2023-10-09 16:56:41 -0700

    corrprintcomb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,22 +2,22 @@
 /**
  * main - Entry point
  *
- * Description: Prints all possible combinations of two different digits
- * separated by a comma and space, in ascending order.
+ * Description: This program prints all possible different combinations of two digits
+ * separated by commas and spaces, in ascending order, followed by a newline.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int first_digit, second_digit;
+	int i, j;
 
-	for (first_digit = 0; first_digit <= 8; first_digit++)
+	for (i = 0; i < 10; i++)
 	{
-		for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
+		for (j = i + 1; j < 10; j++)
 	{
-		putchar('0' + first_digit);
-		putchar('0' + second_digit);
-		if (first_digit != 8 || second_digit != 9)
+		putchar(j + '0');
+		putchar(j + '0');
+		if (i != 8 || j != 9)
 		{
 			putchar(',');
 			putchar(' ');
