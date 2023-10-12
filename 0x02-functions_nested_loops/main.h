958c9f9dd99e3bfa52a9487777da6c959fd8b1e5
Benjamin <owolabenjade@example.com>
2023-10-12 03:09:57 -0700

    0put

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,6 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/* Declare the prototype for the print_alphabet function */
-void print_alphabet(void);
+int _putchar(char c);
 
-int main(void)
-{
-    print_alphabet(); // Call the print_alphabet function
-    return (0);
-}
+#endif /* MAIN_H */
