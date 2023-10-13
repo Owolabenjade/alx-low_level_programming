f067401698ee0921811d784a5a9c31eaeee229a2
Benjamin <owolabenjade@example.com>
2023-10-13 15:55:47 -0700

    realisupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program demonstrates the usage of the _isupper function
+ * by testing it with both uppercase and lowercase characters.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char c;
