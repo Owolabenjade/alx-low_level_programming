9d3b6990a7e34684f9159cb669557d638535f2c9
Benjamin <owolabenjade@example.com>
2023-10-13 16:54:15 -0700

    printline

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program demonstrates the usage of the `print_line` function by calling it with different arguments. The `print_line` function is expected to perform a specific task related to printing lines based on the provided argument.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
