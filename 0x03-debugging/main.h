4c58560074d536af0bdc6c3333842f5a80c62bf2
Benjamin <owolabenjade@example.com>
2023-10-13 18:12:48 -0700

    2mainc

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year)
 
+void print_remaining_days(int month, int day, int year)
 
 #endif /* MAIN_H */
