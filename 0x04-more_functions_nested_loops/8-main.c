334ab670be4f329a3aaa31fd0e646363edeca294
Benjamin <owolabenjade@example.com>
2023-10-13 17:17:41 -0700

    printsquare

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program demonstrates the usage of the `print_square` function by calling it with different arguments. The `print_square` function is expected to perform a specific task related to printing square patterns based on the provided argument, controlling the size of the square.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -25,4 +25,7 @@ void print_line(int n);
 int _putchar(char c);
 void print_diagonal(int n);
 
+int _putchar(char c);
+void print_square(int size);
+
 #endif /* MAIN_H */
