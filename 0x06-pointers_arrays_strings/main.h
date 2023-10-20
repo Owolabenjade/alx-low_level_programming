8b86d8645a66be43d348cb1661bbab1de9189157
Benjamin <owolabenjade@example.com>
2023-10-19 17:53:23 -0700

    11infinteadd

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 void print_number(int n)
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /* MAIN_H */
