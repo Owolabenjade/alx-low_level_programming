fbd013dcf7d389d7dd0d654993e2555e1b90efa4
Benjamin <owolabenjade@example.com>
2023-10-19 17:35:27 -0700

    9printnumnew

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer using _putchar
+ * print_number - Prints an integer
  * @n: The integer to be printed
  */
-	
+
 void print_number(int n)
 {
 	if (n < 0)
