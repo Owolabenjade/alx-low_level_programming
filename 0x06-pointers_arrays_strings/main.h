6828ef39fa86528ed9f05d0f4a32cdfb1fc478ff
Benjamin <owolabenjade@example.com>
2023-10-21 23:11:43 -0700

    new6capstring

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ int _putchar(char c);
 int _strcmp(char *s1, char *s2);  /* Add this line */
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
+char *cap_string(char *str);
 
 #endif /* MAIN_H */
