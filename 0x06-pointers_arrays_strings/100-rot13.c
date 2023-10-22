ec41e928cc25032128a8f4cfce345b9c1afd2e32
Benjamin <owolabenjade@example.com>
2023-10-21 23:28:17 -0700

    new100rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - Encode a string using ROT13.
+ * @str: The input string to encode.
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
