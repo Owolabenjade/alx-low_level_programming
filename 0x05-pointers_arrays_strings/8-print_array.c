5eea0c0e8106f3ed69ed2cabe9dd63cce91a942a
Benjamin <owolabenjade@example.com>
2023-10-23 06:43:02 -0700

    8printarray

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,9 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_array - Prints n elements of an array of integers, followed
- * by a new line.
- * @a: A pointer to an array of integers.
+ * print_array - Prints n elements of an array of integers.
+ * @a: The array of integers.
  * @n: The number of elements to print.
  */
 void print_array(int *a, int n)
@@ -12,52 +12,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		int number = a[i];
-		int isNegative = (number < 0) ? 1 : 0;
+		printf("%d", a[i]);
 
-		if (isNegative)
-		{
-			_putchar('-');
-			number = -number;
-		}
-
-		int numDigits = 0;
-		int temp = number;
-		
-		while (temp != 0)
-		{
-			temp /= 10;
-			numDigits++;
-		}
-
-		if (isNegative)
-		numDigits++;
-
-		int padding = 26 - numDigits;
-
-		while (padding > 0)
-		{
-			_putchar(' ');
-			padding--;
-		}
-
-		if (isNegative)
-		_putchar('-');
-
-		while (number > 0)
-		{
-			int digit = number % 10;
-			
-			_putchar('0' + digit);
-			number /= 10;
-		}
-
-		if (i != n - 1)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		if (i < n - 1)
+			printf(", ");
 	}
 
-	_putchar('\n');
+	printf("\n");
 }
