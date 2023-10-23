5494b1f0cb1bef93b5045a46a3dee4f9b084facc
Benjamin <owolabenjade@example.com>
2023-10-23 07:24:37 -0700

    new101keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,14 +7,14 @@ int main(void)
 {
 	char password[101]; /* Assuming maximum password length of 100 characters */
 	int i;
+	const char valid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 	srand(time(NULL)); /* Seed the random number generator with the current time */
 
 	for (i = 0; i < 100; i++)
 	{
-		int rand_char = rand() % 94 + 33; /* Generate random printable ASCII characters */
-
-		password[i] = (char)rand_char;
+		int rand_index = rand() % (sizeof(valid_chars) - 1);
+		password[i] = valid_chars[rand_index];
 	}
 
 	password[i] = '\0'; /* Null-terminate the password */
