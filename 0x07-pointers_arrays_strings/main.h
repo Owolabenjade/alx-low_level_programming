77d06b4141af424b55d0ab43c53509b36793d7c7
Benjamin <owolabenjade@example.com>
2023-10-24 03:03:22 -0700

    orig2strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,11 +4,6 @@
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 char *_strchr(char *s, char c);
 
