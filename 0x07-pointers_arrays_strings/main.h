33a354e735db4c2009fc6414527c07e268ca73d7
Benjamin <owolabenjade@example.com>
2023-10-24 03:23:30 -0700

    new5strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,4 +13,7 @@ unsigned int _strspn(char *s, char *accept);
 int _putchar(char c);
 char *_strpbrk(char *s, char *accept);
 
+int _putchar(char c);
+char *_strstr(char *haystack, char *needle);
+
 #endif /* MAIN_H */
