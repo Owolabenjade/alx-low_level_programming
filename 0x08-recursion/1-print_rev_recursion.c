de38a9b99ea815145d9f5d80515f2b1995cadd91
Benjamin <owolabenjade@example.com>
2023-10-25 18:10:13 -0700

    new1printrevrec

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,16 +2,6 @@
 #include "main.h" /* Include the header file */
 
 /**
- * _putchar - Custom function to print a character
- * (Assuming you have a _putchar function;
- * you can replace this with your output function).
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
-
-/**
  * _print_rev_recursion - Prints a string in reverse recursively.
  * @s: The string to be printed in reverse.
  */
@@ -23,5 +13,5 @@ void _print_rev_recursion(char *s)
 	}
 
 	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	putchar(*s);
 }
--- 0x08-recursion/1main.c
+++ 0x08-recursion/1main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h" /* Include the header file */
 
-int main(void) {
+int main(void)
+{
 	char str[] = "Hello, world!";
 	_print_rev_recursion(str);
 	printf("\n"); /* Print a newline to separate output */
