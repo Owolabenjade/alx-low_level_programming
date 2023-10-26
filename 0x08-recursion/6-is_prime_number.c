37d92105c243d2add08150479ae262637ac62162
Benjamin <owolabenjade@example.com>
2023-10-25 19:37:43 -0700

    orig6isprimenum

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,26 +9,26 @@
  * If 'n' is less than or equal to 1, the function returns 0(not prime).
  * If 'n' is 2(the only even prime number), the function returns 1 (prime).
  * For 'n' greater than 2, it checks for divisibility by all odd numbers
- * from 3 to the square root of 'n'. If 'n' is divisible by any of these numbers,
- * it returns 0. Otherwise, it returns 1, indicating
+ * from 3 to the square root of 'n'. If 'n' is divisible by any of these
+ * numbers, it returns 0. Otherwise, it returns 1, indicating
  * 'n' is a prime number.
  *
  * Return: 1 if 'n' is a prime number, 0 otherwise.
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
+	if (n <= 1)
 	{
-        return (0); /* Not prime */
-    }
+		return (0); /* Not prime */
+	}
 	else if (n == 2)
 	{
-        return (1); /* 2 is prime */
-    }
+		return (1); /* 2 is prime */
+	}
 	else
 	{
-        return (is_prime_recursive_helper(n, 3));
-    }
+		return (is_prime_recursive_helper(n, 3));
+	}
 }
 
 /**
@@ -36,10 +36,12 @@ int is_prime_number(int n)
  * @n: The integer to be checked for primality.
  * @divisor: The current divisor to check for divisibility.
  *
- * This recursive helper function checks if 'n' is divisible by 'divisor' without
- * any remainder. It iterates through odd divisors from 3 to the square root of 'n'
- * and returns 1 if 'n' is not divisible by any of these divisors, indicating 'n' is prime.
- * Otherwise, it returns 0 if 'n' is divisible by any divisor.
+ * This recursive helper function checks if 'n' is divisible by
+ * 'divisor' without any remainder. It iterates through
+ * odd divisors from 3 to the square root of 'n'
+ * and returns 1 if 'n' is not divisible by any of these divisors,
+ * indicating 'n' is prime. Otherwise, it returns 0 if 'n'
+ * is divisible by any divisor.
  *
  * Return: 1 if 'n' is a prime number, 0 otherwise.
  */
