5886eb7db69891b55d07900674a206faf9275984
Benjamin <owolabenjade@example.com>
2023-10-25 17:42:44 -0700

    bestorig0putsrec

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include "main.h" // Include the header file
+#include "main.h" /* Include the header file */
 
 /**
  * puts_recursion.c
  *
  * This source file contains the implementation of the _puts_recursion function.
- * It includes the main header file to access the function prototype for _puts_recursion.
- * The _puts_recursion function recursively prints a string followed by a newline, without using loops.
+ * It includes the main header file to access the 
+ * function prototype for _puts_recursion.
+ * The _puts_recursion function recursively prints
+ * a string followed by a newline, without using loops.
  */
  
 void _puts_recursion(char *s)
