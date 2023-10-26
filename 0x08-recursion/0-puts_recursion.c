0c93f651d26772c754e4dffca375361e71f02f4d
Benjamin <owolabenjade@example.com>
2023-10-25 17:51:23 -0700

    0putsrecnew

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,23 @@
 #include "main.h" /* Include the header file */
 
 /**
- * puts_recursion.c
+ * _puts_recursion - Prints a string recursively followed by a newline.
+ * @s: The string to be printed.
  *
- * This source file contains the implementation of the _puts_recursion function.
+ * This function takes a string 's' and recursively prints each character
+ * until the end of the string is reached. It adds a newline character at
+ * the end to create a newline in the output.
+ * This source file contains the implementation of the _puts_recursion function
  * It includes the main header file to access the 
  * function prototype for _puts_recursion.
  * The _puts_recursion function recursively prints
  * a string followed by a newline, without using loops.
  */
- 
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') {
+	if (*s == '\0')
+	{
 		putchar('\n');
 		return;
 	}
