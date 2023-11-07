9d76a49a25445747d80556ed835c8bda6839d696
Benjamin <owolabenjade@example.com>
2023-11-07 06:14:27 -0800

    4newdogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+typedef struct dog
+{
+	    char *name;
+	        float age;
+		    char *owner;
+} dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
 struct dog
 {
 		char *name;
