5af5895fb239367c01049c9eda31ab1a34da2698
Benjamin <owolabenjade@example.com>
2023-11-07 05:54:54 -0800

    4new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,7 +10,8 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-typedef struct dog dog_t;  /* Define dog_t as an alias for struct dog */
+typedef struct dog dog_t;  /* Define dog_t as an alias for struc */
+dog_t *new_dog(char *name, float age, char *owner)
 void print_dog(struct dog *d);
 
 #endif /* DOG_H */
