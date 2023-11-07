7e46d3897b9eceb560753a6bd896cd3442bec3c3
Benjamin <owolabenjade@example.com>
2023-11-07 02:13:10 -0800

    2mainc

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,7 @@
-/**
- *  * main - Entry point of the program
- *   *
- *    * Description:
- *     *   This program prints the name of the source file it was compiled from.
- *      *   It utilizes the __FILE__ predefined macro to obtain the file name and
- *       *   prints it to the standard output.
- *        *
- *         * Return:
- *          *   0 - Indicates successful execution
- *           */
+#include <stdio.h>
+
 int main(void)
 {
-		printf("%s\n", __FILE__);  /* Print the source file name */
-			return 0;  /* Return 0 to indicate successful execution */
+	printf("%s\n", __FILE__);
+	    return 0;
 }
