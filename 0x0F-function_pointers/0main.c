b654890e82a34283092051a49e21aa3b678e69d2
Benjamin <owolabenjade@example.com>
2023-11-08 09:46:23 -0800

    Ofunction

---
+++ 0x0F-function_pointers/0main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/* Example function that prints a string */
+void print_string(char *str)
+{
+		printf("Name: %s\n", str);
+}
+
+int main(void)
+{
+		char *name = "John Doe";
+
+			print_name(name, print_string);
+
+				return (0);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+ANother reading again
