dc8765ee3b7a3de2f70e5d924995fefba820b631
Benjamin <owolabenjade@example.com>
2023-11-08 15:45:06 -0800

    new0funcpoint

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,23 +1,13 @@
-/**
- * print_name - Prints a name using a function pointer
- * @name: The name to be printed
- * @f: A pointer to a function that prints a string
- *
- * This function takes a name and a function pointer
- * and uses the function pointer
- * to print the name. It checks if both the name
- * and the function pointer are not NULL
- * before calling the function pointed to
- * by 'f'. If 'name' or 'f' is NULL, the function
- *         * does nothing.
- *          */
-
 #include "function_pointers.h"
+#include <stdio.h>
 
+/**
+ *  * print_name - Prints a name using a function pointer
+ *   * @name: The name to print
+ *    * @f: A function that prints a string
+ *     */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-	{
+	if (name && f)
 		f(name);
-	}
 }
