84d6183b24dad2e4ade77d8eadf39faee92b7bae
Owolabenjade <obeejay99@gmail.com>
2023-11-19 13:36:49 -0800

    letmego

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *@argc: Number of arguments
+ *@argv: Array of pointers to strings containing the arguments
+ *
+ *Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
+int main(int argc, char *argv[]);
 
+#include <stddef.h>
 int main(void);
 
 #endif /* MAIN_H */
