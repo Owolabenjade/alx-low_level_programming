faa230eb58914c0f481df1b3a74d72ea0e51f7a9
Owolabenjade <obeejay99@gmail.com>
2023-11-19 13:56:40 -0800

    justbe

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,3 +11,17 @@ int whatsmyname(void)
 		printf("%s\n", __FILE__);
 			return (0);
 }
+
+/**
+ *  * main - Entry point of the program for Task 1
+ *   * @argc: The number of command-line arguments
+ *    * @argv: An array containing the command-line arguments
+ *     *
+ *      * Return: Always 0
+ *       */
+int main(int argc, char *argv[])
+{
+		(void)argv; /* Unused parameter warning */
+			printf("%d\n", argc - 1);
+				return (0);
+}
