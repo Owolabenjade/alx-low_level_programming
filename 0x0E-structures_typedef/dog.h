0b377cab8d86c9d206fc9fc6766207f87459e9a6
Owolabenjade <obeejay99@gmail.com>
2023-11-19 15:32:42 -0800

    oniduro

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,9 +23,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-/**
- *dog_t - Typedef for struct dog.
-*/
+/* Typedef for struct dog. */
 
 typedef struct dog dog_t;
 
