8db9f91d14a80cb29347dad0e7cfbfb6304ed4ff
Owolabenjade <obeejay99@gmail.com>
2023-11-19 13:18:12 -0800

    abeg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
-nclude "main.h"
+#include "main.h"
+#include <stddef.h>
 #include <stdio.h>
 
 /**
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+
 int main(void);
 
 #endif /* MAIN_H */
