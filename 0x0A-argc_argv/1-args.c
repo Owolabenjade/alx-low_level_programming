506f99ddd8ac0446f8e92d9992f1cfab6e5644e7
Owolabenjade <obeejay99@gmail.com>
2023-11-19 13:44:48 -0800

    hope

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,6 +10,7 @@
 */
 int main(int argc, char *argv[])
 {
+	(void)argv; /* Unused parameter warning */
 	printf("%d\n", argc - 1);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int main(int argc, char *argv[]);
-
-#include <stddef.h>
-int main(void);
+/* #include <stddef.h>
+int main(void); */
 
 #endif /* MAIN_H */
