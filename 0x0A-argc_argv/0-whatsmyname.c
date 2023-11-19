1f7c2ab7dd58c44f1ee461a7565c366b7c53c9c7
Owolabenjade <obeejay99@gmail.com>
2023-11-19 13:12:34 -0800

    letmebreath

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+#include <stdio.h>
+
+/**
+ *  * main - Entry point of the program
+ *   *
+ *    * Return: Always 0
+ *     */
+int main(void)
+{
+		printf("%s\n", __FILE__);
+			return (0);
+}
