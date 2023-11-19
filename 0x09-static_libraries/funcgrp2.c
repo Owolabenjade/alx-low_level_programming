9526fba6ec42fd558c63e539ea1f30f412d5a4ea
Owolabenjade <obeejay99@gmail.com>
2023-11-19 09:04:59 -0800

    latecommmer

---
+++ 0x09-static_libraries/funcgrp2.c
@@ -0,0 +1,39 @@
+/* _strlen.c */
+#include "main.h"
+
+int _strlen(char *s)
+{
+		/* Your implementation */
+}
+
+/* _puts.c */
+#include "main.h"
+
+void _puts(char *s)
+{
+		/* Your implementation */
+}
+
+/* _strcpy.c */
+#include "main.h"
+
+char *_strcpy(char *dest, char *src)
+{
+		/* Your implementation */
+}
+
+/* _atoi.c */
+#include "main.h"
+
+int _atoi(char *s)
+{
+		/* Your implementation */
+}
+
+/* _strcat.c */
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+		/* Your implementation */
+}
---
+++ 0x09-static_libraries/funcgrp4.c
@@ -0,0 +1,31 @@
+/* _strchr.c */
+#include "main.h"
+
+char *_strchr(char *s, char c)
+{
+		/* Your implementation */
+}
+
+/* _strspn.c */
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+		/* Your implementation */
+}
+
+/* _strpbrk.c */
+#include "main.h"
+
+char *_strpbrk(char *s, char *accept)
+{
+		/* Your implementation */
+}
+
+/* _strstr.c */
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+		/* Your implementation */
+}
