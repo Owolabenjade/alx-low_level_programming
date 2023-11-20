e0ab74a4614bd30762e0f33f8bb8213ea883d349
Owolabenjade <obeejay99@gmail.com>
2023-11-20 01:36:47 -0800

    listint

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,7 +17,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Function prototype for 0-print_listint.c */
+/* Function prototypes for 0-print_listint.c and 1-listint_len.c */
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
