1e92517f07154285df3aa6c787adf872606b7953
Owolabenjade <obeejay99@gmail.com>
2023-11-21 12:32:40 -0800

    odayanmo

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+/* lists.h */
+
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/* Define the structure for a node in the list */
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+} list_t;
+
+/* Function prototype for printing the list */
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
