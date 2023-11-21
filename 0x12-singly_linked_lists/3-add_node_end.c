21d7e129f5bf53f782456bbd9a149de4fc74536a
Owolabenjade <obeejay99@gmail.com>
2023-11-21 14:36:19 -0800

    emilejo

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,6 +3,32 @@
 #include <string.h> /* for strdup */
 
 /**
+ *create_node - Creates a new node with the given string.
+ *@str: The string to be added to the new node.
+ *Return: The address of the new node, or NULL if it failed.
+*/
+static list_t *create_node(const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	return (new_node);
+}
+
+/**
  *add_node_end - Adds a new node at the end of a list_t list.
  *@head: A pointer to a pointer to the head of the list.
  *@str: The string to be added to the new node.
@@ -13,42 +39,23 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 	list_t *last_node;
 
-	/* Check for invalid input */
 	if (str == NULL || head == NULL)
 		return (NULL);
 
-	/* Allocate memory for the new node */
-	new_node = malloc(sizeof(list_t));
+	new_node = create_node(str);
 	if (new_node == NULL)
 		return (NULL);
 
-	/* Duplicate the string using strdup */
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	/* Set the length of the string */
-	new_node->len = strlen(str);
-
-	/* Set next of new node to NULL as it will be the last node */
-	new_node->next = NULL;
-
-	/* If the list is empty, make the new node the head */
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
 
-	/* Find the last node in the list */
 	last_node = *head;
 	while (last_node->next != NULL)
 		last_node = last_node->next;
 
-	/* Attach the new node at the end of the list */
 	last_node->next = new_node;
 
 	return (new_node);
