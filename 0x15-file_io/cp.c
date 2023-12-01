d9973f77f68c4da502d877aef7d4ce4d3255e8fd
Owolabenjade <obeejay99@gmail.com>
2023-12-01 15:13:51 -0800

    3second

--- 0x15-file_io/cp.c
+++ 0x15-file_io/cp.c
@@ -1,26 +1,66 @@
-#include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+nclude "main.h"
 
 /**
- *main - Copies the content of a file to another file.
- *@argc: The number of arguments.
- *@argv: The array of arguments.
- *
- *Return: 0 on success, otherwise exit with the specified error codes.
-*/
-int main(int argc, char *argv[])
+ *  * error_exit - Exits the program with an error code and prints an error message.
+ *   * @code: The exit code.
+ *    * @message: The error message format.
+ *     * @...: Additional arguments for the error message format.
+ *      */
+void error_exit(int code, const char *message, ...)
 {
-	if (argc != 3)
-	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
+		va_list args;
+			va_start(args, message);
+				dprintf(STDERR_FILENO, "Error: ");
+					vdprintf(STDERR_FILENO, message, args);
+						dprintf(STDERR_FILENO, "\n");
+							va_end(args);
+								exit(code);
+}
 
-	if (copy_file(argv[1], argv[2]) == -1)
-		exit(99);
+/**
+ *  * copy_file - Copies the content of a file to another file.
+ *   * @file_from: The source file.
+ *    * @file_to: The destination file.
+ *     */
+void copy_file(const char *file_from, const char *file_to)
+{
+		int fd_from, fd_to, read_result, write_result;
+			char buffer[BUFFER_SIZE];
 
-	return (0);
+				fd_from = open(file_from, O_RDONLY);
+					if (fd_from == -1)
+								error_exit(98, "Can't read from file %s", file_from);
+
+						fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+							if (fd_to == -1)
+									{
+												close(fd_from);
+														error_exit(99, "Can't write to file %s", file_to);
+															}
+
+								do
+										{
+													read_result = read(fd_from, buffer, BUFFER_SIZE);
+															if (read_result == -1)
+																		{
+																						close(fd_from);
+																									close(fd_to);
+																												error_exit(98, "Can't read from file %s", file_from);
+																														}
+
+																	write_result = write(fd_to, buffer, read_result);
+																			if (write_result == -1 || write_result != read_result)
+																						{
+																										close(fd_from);
+																													close(fd_to);
+																																error_exit(99, "Can't write to file %s", file_to);
+																																		}
+
+																				} while (read_result > 0);
+
+									if (close(fd_from) == -1)
+												error_exit(100, "Can't close fd %d", fd_from);
+
+									if (close(fd_to) == -1)
+												error_exit(100, "Can't close fd %d", fd_to);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,16 +9,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_exit(int code, const char *message, ...);
+void copy_file(const char *file_from, const char *file_to);
 
 
-#define BUFFER_SIZE 1024
-
-int _putchar(char c);
-void print_error(int code, const char *file);
-int copy_file(const char *file_from, const char *file_to);
-
-
-
-
-
-#endif /* MAIN_H */
+rendif /* MAIN_H */
