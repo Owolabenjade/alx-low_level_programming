f0f0992d24e824450e1c9b84b81d78a163b5eb87
Owolabenjade <obeejay99@gmail.com>
2023-12-06 15:32:31 -0800

    2fourth

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -13,4 +12,4 @@ void error_exit(int code, const char *message, ...);
 void copy_file(const char *file_from, const char *file_to);
 
 
-rendif /* MAIN_H */
+#endif /* MAIN_H */
