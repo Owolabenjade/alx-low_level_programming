cc25ec22b8d0c4fceb49b0b8887b1ddf48162626
Benjamin <owolabenjade@gmail.com>
2024-02-22 11:08:29 -0800

    baby

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -30,8 +30,8 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-/* ... Other existing prototypes ... */
-
+/* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /* HASH_TABLES_H */
