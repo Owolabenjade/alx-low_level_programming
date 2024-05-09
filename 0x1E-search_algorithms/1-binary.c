60676409e29d2265b385b7d548b03e6b9aca48d6
Benjamin <owolabenjade@gmail.com>
2024-05-09 10:18:25 -0700

    eru

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,19 +1,37 @@
 #include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * print_array - Helper function to print the current subarray being searched.
+ * @array: Pointer to the first element of the subarray.
+ * @left: Starting index of the subarray.
+ * @right: Ending index of the subarray.
+ */
+void print_array(const int *array, size_t left, size_t right)
+{
+    printf("Searching in array: ");
+    while (left <= right)
+    {
+        printf("%d", array[left]);
+        if (left < right)
+            printf(", ");
+        left++;
+    }
+    printf("\n");
+}
 
 /**
  * binary_search - Searches for a value in a sorted array of integers
  *                 using the Binary search algorithm.
  * @array: Pointer to the first element of the array to search in.
  * @size: Number of elements in the array.
- * @value: Value to search for.
- * 
- * Return: Index where value is located, or -1 if not found or array is NULL.
+ * @value: The value to search for.
+ *
+ * Return: The index where value is located or -1 if not found or if array is NULL.
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid;
+    size_t left = 0, right = size - 1, mid;
 
     if (array == NULL)
         return -1;
@@ -24,31 +42,12 @@ int binary_search(int *array, size_t size, int value)
         mid = left + (right - left) / 2;
 
         if (array[mid] == value)
-            return mid;
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
+            return (int)mid;
+        else if (array[mid] > value)
             right = mid - 1;
+        else
+            left = mid + 1;
     }
-
     return -1;
-}
-
-/**
- * print_array - Prints the array from left index to right index.
- * @array: The array to be printed.
- * @left: Left index of the subarray.
- * @right: Right index of the subarray.
- */
-void print_array(const int *array, size_t left, size_t right)
-{
-    printf("Searching in array: ");
-    for (size_t i = left; i <= right; i++)
-    {
-        printf("%d", array[i]);
-        if (i < right)
-            printf(", ");
-    }
-    printf("\n");
 }
 
