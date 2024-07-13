72da11802e6863e97836d2c5f782b6ed818bf023
Benjamin <owolabenjade@gmail.com>
2024-07-13 03:54:03 -0700

    sorry

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,36 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalize all words in a string.
- * @str: The input string.
- * Return: A pointer to the modified string.
+ * cap_string - Capitalizes all words of a string
+ * @str: The string to be capitalized
+ *
+ * Return: A pointer to the changed string
  */
 char *cap_string(char *str)
 {
-	int i;
+    int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i == 0 || (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-					   str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-					   str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
-					   str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
-					   str[i - 1] == '}'))
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 'a' + 'A';
-		}
-	}
+    while (str[i])
+    {
+        /* Capitalize if it's the first character */
+        if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+            str[i] -= 32;
 
-	return (str);
+        /* Check for separators */
+        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+            str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+            str[i] == '}')
+        {
+            /* Capitalize the next character if it's a letter */
+            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+                str[i + 1] -= 32;
+        }
+
+        i++;
+    }
+
+    return (str);
 }
+
