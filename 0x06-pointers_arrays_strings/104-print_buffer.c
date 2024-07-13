3a2a0464307a331105e752f13f7854f0c6c7fc88
Benjamin <owolabenjade@gmail.com>
2024-07-12 17:49:51 -0700

    oluwa

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,51 +1,44 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - Print the content of a buffer in a specific format.
- * @b: The buffer to print.
- * @size: The number of bytes to print.
+ * print_buffer - prints a buffer
+ * @b: buffer.
+ * @size: size of buffer.
+ * Return: no return.
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x: ", i);
+	int j, k, l;
 
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j < size)
-				printf("%02x", b[i + j]);
-			else
-				printf("  ");
-
-			if (j % 2 != 0)
-				printf(" ");
-		}
-
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j < size)
-			{
-				if (b[i + j] >= 32 && b[i + j] <= 126)
-					printf("%c", b[i + j]);
-				else
-					printf(".");
-			}
-			else
-				printf(" ");
-		}
+	if (size <= 0)
 		printf("\n");
+	else
+	{
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
 	}
 }
 
-int main(void)
-{
-	char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
-
-	printf("%s\n", buffer);
-	printf("---------------------------------\n");
-	print_buffer(buffer, sizeof(buffer));
-	return (0);
-}
