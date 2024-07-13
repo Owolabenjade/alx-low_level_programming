ed5f5e86d23932f2fe5dde1ccaab6fd648e5871c
Benjamin <owolabenjade@gmail.com>
2024-07-13 08:40:52 -0700

    hard

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,37 +1,33 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _calloc - Allocates memory for an array and initializes to zero
- * @nmemb: Number of elements in the array
- * @size: Size in bytes of each element
+ * _calloc - Allocates memory for an array, using malloc
+ * @nmemb: Number of elements
+ * @size: Size of each element
  *
- * Description:
- * This function allocates memory for an array of 'nmemb' elements, each of
- * 'size' bytes, and initializes the memory to zero. If 'nmemb' or 'size' is
- * zero, the function returns 'NULL'. If 'malloc' fails to allocate memory,
- * the function also returns 'NULL'.
- *
- * Return: A pointer to the newly allocated and initialized memory or 'NULL' on
- * failure.
+ * Return: Pointer to allocated memory, or NULL if it fails
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
 	unsigned int i;
-	char *cptr;
 
+	/* Check if nmemb or size is 0 */
 	if (nmemb == 0 || size == 0)
-		return NULL;
+		return (NULL);
 
+	/* Allocate memory */
 	ptr = malloc(nmemb * size);
 
+	/* Check if malloc failed */
 	if (ptr == NULL)
-		return NULL;
+		return (NULL);
 
-	cptr = (char *)ptr;
-	for (i = 0; i < nmemb * size; i++)
-		cptr[i] = 0;
+	/* Set memory to zero */
+	for (i = 0; i < (nmemb * size); i++)
+	ptr[i] = 0;
 
 	return (ptr);
 }
+
