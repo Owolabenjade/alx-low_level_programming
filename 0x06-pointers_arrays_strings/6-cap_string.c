01732a4a2aef6e31e74d5864a29d6c8676e33114
Benjamin <owolabenjade@gmail.com>
2024-07-13 04:00:22 -0700

    march

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,29 +8,29 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i])
-    {
+	while (str[i])
+	{
         /* Capitalize if it's the first character */
-        if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-            str[i] -= 32;
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		str[i] -= 32;
 
         /* Check for separators */
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-            str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-            str[i] == '}')
-        {
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		str[i] == '}')
+	{
             /* Capitalize the next character if it's a letter */
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] -= 32;
-        }
+		if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		str[i + 1] -= 32;
+	}
 
-        i++;
-    }
+	i++;
+	}
 
-    return (str);
+	return (str);
 }
 
