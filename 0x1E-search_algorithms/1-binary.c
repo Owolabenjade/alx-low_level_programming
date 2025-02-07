9508c2095d23a03256d8ecfe5c069af29c0eaffd
Benjamin <obeejay99@gmail.com>
2025-02-07 08:45:08 +0100

    bebe

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_array - Helper function to print the current subarray being searched.
- * @array: Pointer to the first element of the subarray.
- * @left: Starting index of the subarray.
- * @right: Ending index of the subarray.
- */
+* print_array - Helper function to print the current subarray being searched.
+* @array: Pointer to the first element of the subarray.
+* @left: Starting index of the subarray.
+* @right: Ending index of the subarray.
+*/
 void print_array(const int *array, size_t left, size_t right)
 {
 	printf("Searching in array: ");
@@ -22,20 +22,19 @@ void print_array(const int *array, size_t left, size_t right)
 
 /**
  * binary_search - Searches for a value in a sorted array of integers
- *                 using the Binary search algorithm.
+ * using the Binary search algorithm.
  * @array: Pointer to the first element of the array to search in.
  * @size: Number of elements in the array.
  * @value: The value to search for.
- *
  * Return: The index where value is located or
  * -1 if not found or if array is NULL.
- */
+*/
 int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0, right = size - 1, mid;
 
 	if (array == NULL)
-		(return -1);
+		return -1;
 
 	while (left <= right)
 	{
@@ -43,12 +42,11 @@ int binary_search(int *array, size_t size, int value)
 		mid = left + (right - left) / 2;
 
 		if (array[mid] == value)
-			(return (int)mid);
+			return (int)mid;
 		else if (array[mid] > value)
 			right = mid - 1;
 		else
 			left = mid + 1;
 	}
-	(return -1);
+	return -1;
 }
-
