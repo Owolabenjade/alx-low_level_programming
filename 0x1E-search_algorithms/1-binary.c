29b4b8b28b4dc9523d5120e20e566826a0252a5b
Benjamin <obeejay99@gmail.com>
2025-02-07 08:29:02 +0100

    pray

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -27,14 +27,15 @@ void print_array(const int *array, size_t left, size_t right)
  * @size: Number of elements in the array.
  * @value: The value to search for.
  *
- * Return: The index where value is located or -1 if not found or if array is NULL.
+ * Return: The index where value is located or
+ * -1 if not found or if array is NULL.
  */
 int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0, right = size - 1, mid;
 
 	if (array == NULL)
-		return -1;
+		(return -1);
 
 	while (left <= right)
 	{
@@ -42,12 +43,12 @@ int binary_search(int *array, size_t size, int value)
 		mid = left + (right - left) / 2;
 
 		if (array[mid] == value)
-			return (int)mid;
+			(return (int)mid);
 		else if (array[mid] > value)
 			right = mid - 1;
 		else
 			left = mid + 1;
 	}
-	return -1;
+	(return -1);
 }
 
